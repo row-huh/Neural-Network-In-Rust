2c9ecc330c44dc8e02a3c7b95cf48a75e303237e
Roha Pathan <rohaabropathan@gmail.com>
2026-03-02 21:44:12 +0500

    created type nn and added random weights

--- tiny-nn/src/main.rs
+++ tiny-nn/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+struct NeuralNetwork {
+    weights: Vec<f64>
+}
+
+impl NeuralNetwork {
+    fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let weights = vec![rng.gen_range(0.0..1.0), rng.gen_range(0.0..1.0)]
+
+
+        Self {
+            weights
+        }
+    }
 }
