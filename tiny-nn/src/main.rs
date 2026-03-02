59f424df729b9b4d905a22c57c236480d2c3e305
Roha Pathan <rohaabropathan@gmail.com>
2026-03-02 22:12:32 +0500

    added activation function

--- tiny-nn/src/main.rs
+++ tiny-nn/src/main.rs
@@ -1,15 +1,34 @@
 struct NeuralNetwork {
     weights: Vec<f64>
+    bias f64
 }
+
+// output = activation(weights* inputs + bias)
 
 impl NeuralNetwork {
     fn new() -> Self {
         let mut rng = rand::thread_rng();
-        let weights = vec![rng.gen_range(0.0..1.0), rng.gen_range(0.0..1.0)]
+        weights = vec![rng.gen_range(0.0..1.0), rng.gen_range(0.0..1.0)]
 
 
         Self {
-            weights
+            weights,
+            bias: rng.gen_range(0.0..1.0)
         }
     }
+
+    // parameter is the object itself and a vector of type f64 with 2 items (which are the inputs)
+
+    fn predict(&Self, input:&[f64, 2]) -> f64 {
+        for (i, weight) in Self.weights.iter().enumerate() {
+            sum += input[i] * weight;
+        }
+
+        sigmoid(sum)
+    }
+}
+
+
+fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + (-x).exp())
 }
